23cb491059cbf80f80a566fbcedaf980fd1e74e2
Denis S Dujota <denisdujota@gmail.com>
2024-11-13 04:33:12 +0300

    Add middleware for add user to views and is signed in

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const dotenv = require('dotenv');
+
 dotenv.config();
 const express = require('express');
+
 const app = express();
 const mongoose = require('mongoose');
+
+// Middleware
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
 
+// Controllers
 const authController = require('./controllers/auth.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
@@ -19,7 +26,7 @@ mongoose.connection.on('connected', () => {
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -27,22 +34,17 @@ app.use(
     saveUninitialized: true,
   })
 );
+app.use(passUserToView);
 
+// Public Routes
 app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
-
-app.get('/vip-lounge', (req, res) => {
-  if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
-  } else {
-    res.send('Sorry, no guests allowed.');
-  }
+  res.render('index.ejs');
 });
 
 app.use('/auth', authController);
+
+// Protected Routes
+app.use(isSignedIn); // anything below this point will need a user to be signed in
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
